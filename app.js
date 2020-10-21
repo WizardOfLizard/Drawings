6051bcfa1bce41bc855a302cea26e5519bf497e1
Daniel Lawrence Hensler <henslerdl@s.dcsdk12.org>
2020-10-21 09:23:42 -0600

    Finished stick figure animation.

--- app.js
+++ app.js
@@ -31,6 +31,34 @@ function setup () {
 function draw () {
     if (drawing === 1) {
         background(255, 255, 255)
+
+        fill(255, 255, 255)
+        stroke(0, 0, 0)
+        strokeWeight(5)
+
+        line(350, 150, 350, 300)
+        line(350, 225, 300+stickXOffset, 225+stickYOffset)
+        line(350, 225, 400-stickXOffset, 225-stickYOffset)
+        line(350, 300, 325, 375)
+        line(350, 300, 375, 375)
+        ellipse(350, 150, 50, 50)
+
+        if (stickXOffset >= 50) {
+            stickXVel = -5
+        }
+        if (stickXOffset <= -25) {
+            stickXVel = 5
+        }
+        if (stickYOffset >= 50) {
+            stickYVel = -5
+        }
+        if (stickYOffset <= -50) {
+            stickYVel = 5
+        }
+
+        stickXOffset += stickXVel
+        stickYOffset += stickYVel
+
         //console.log("now stick")
     } else if (drawing === 2) {
         background(0, 0, 0)
